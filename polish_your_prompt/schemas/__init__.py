40a6d951173c5ccf58b6f89c82ece796d7f0d72c
chenzihong <522023320011@smail.nju.edu.cn>
2024-07-11 20:59:57 +0800

    fix: add schemas

--- polish_your_prompt/schemas/__init__.py
+++ polish_your_prompt/schemas/__init__.py
@@ -1,4 +1,35 @@
 from .schema import Schema
+from .APE import APE
+from .BROKE import BROKE
+from .CARE import CARE
+from .CHAT import CHAT
 from .COSTAR import COSTAR
+from .CREATE import CREATE
+from .CRISPE import CRISPE
+from .ERA import ERA
+from .ICIO import ICIO
+from .RACE import RACE
+from .RISE import RISE
+from .ROSES import ROSES
+from .RTF import RTF
+from .TAG import TAG
+from .TRACE import TRACE
 
-__all__ = ["Schema", "COSTAR"]
+__all__ = [
+    "Schema",
+    "APE",
+    "BROKE",
+    "CARE",
+    "CHAT",
+    "COSTAR",
+    "CREATE",
+    "CRISPE",
+    "ERA",
+    "ICIO",
+    "RACE",
+    "RISE",
+    "ROSES",
+    "RTF",
+    "TAG",
+    "TRACE",
+]
